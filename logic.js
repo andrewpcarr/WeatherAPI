4a27ba7d73da79c5eabc6f2576c6aa53f76018da
Andrew Carr <andrewpcarr@gmail.com>
2017-02-15 16:36:14 -0600

    initial files

---
+++ logic.js
@@ -0,0 +1,44 @@
+// Grabs the data from the respective field
+$( document ).ready(function() {
+    var appID = "0787e411c2c3f2a9bae761182f2e042f";
+
+    $(".query_btn").click(function(){
+
+        var query_param = $(this).prev().val();
+		
+		// Functionality -- checks which input is being trasnmitted to the API
+        if ($(this).prev().attr("placeholder") == "City") {
+            var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
+        } else if ($(this).prev().attr("placeholder") == "Zip Code") {
+            var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&APPID=" + appID;
+        }
+
+        //
+        $.getJSON(weather,function(json){
+            $("#city").html(json.name);
+            $("#main_weather").html(json.weather[0].main);
+            $("#description_weather").html(json.weather[0].description);
+            $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+            $("#temperature").html(json.main.temp);
+            $("#pressure").html(json.main.pressure);
+            $("#humidity").html(json.main.humidity);
+        });
+    })
+
+    // Optional Code for temperature conversion
+    var fahrenheit = true;
+
+    $("#convertToCelsius").click(function() {
+        if (fahrenheit) {
+            $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
+        }
+        fahrenheit = false;
+    });
+
+    $("#convertToFahrenheit").click(function() {
+        if (fahrenheit == false) {
+            $("#temperature").text((($("#temperature").text() * (9/5)) + 32));
+        }
+        fahrenheit = true;
+    });
+});
